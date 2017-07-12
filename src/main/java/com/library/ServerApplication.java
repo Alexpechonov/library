75aba253d7bacf6229fbdc89c7b3e9d4d652c9b8
Alexpechonov <lexa19971997@tut.by>
2017-07-12 20:54:34 +0300

    Initial ver.2

---
+++ src/main/java/com/library/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.library; /**
+ * Created by user on 05.07.2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
